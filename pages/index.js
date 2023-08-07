5cc361085a649bd87e337e0fc36368992133b6f2
Kaung Soe San <kaungsoesan01@gmail.com>
2023-08-08 00:13:20 +0630

    add checking user to limit play time to only once

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,60 @@
 import { Box, Typography } from "@mui/material";
 import CustomButton from "../components/CustomButton";
 import Input from "../components/Input";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useRouter } from "next/router";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
 
 export default function SignUpPage() {
   const [username, setUsername] = useState("");
   const router = useRouter();
+  const [open, setOpen] = React.useState(false);
 
-  const storeUsername = async (event) => {
-    event.preventDefault();
-
-    if (!isUserAlreadyExists) {
-      const data = {
-        username: username,
-      };
-
-      const JSONdata = JSON.stringify(data);
-
-      const endpoint = "/api/users";
-
-      const options = {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSONdata,
-      };
-
-      const result = await fetch(endpoint, options);
-    }
+  const handleClickOpen = () => {
+    setOpen(true);
   };
 
-  // useEffect(() => {
-  //   const isUserAlreadyExists = () => {
-  //     return getUserWithUsername() ? true : false;
-  //   };
-  // });
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-  const goToTutorial = () => {
-    router.push({
-      pathname: "/tutorial/[username]",
-      query: { username: username },
-    });
+  const isUserAlreadyExists = async () => {
+    const endpoint = `/api/users/${username}`;
+
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const response = await fetch(endpoint, options);
+    const result = await response.json();
+
+    return (await result) ? true : false;
+  };
+
+  const goToTutorial = async () => {
+    if (!(await isUserAlreadyExists())) {
+      router.push({
+        pathname: "/tutorial/[username]",
+        query: { username: username },
+      });
+    } else {
+      handleClickOpen();
+      setUsername("");
+    }
   };
 
   return (
@@ -77,6 +89,23 @@ export default function SignUpPage() {
         />
         <CustomButton text="Done" onClickHandler={goToTutorial} />
       </Box>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>Oops! We are sorry!</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            You have used your chance to play!
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Okay, but I'm so sad.</Button>
+        </DialogActions>
+      </Dialog>
     </Box>
   );
 }
